0549c72475af61ae51d03ed3ebf8e95bdc940c3d
Mustakim <mustakimsayyed50@gmail.com>
2025-12-15 22:36:53 +0000

    add integration-test for usercontroller

--- src/test/java/com/app/ecom/integrationtest/UserControllerIT.java
+++ src/test/java/com/app/ecom/integrationtest/UserControllerIT.java
@@ -1,14 +1,24 @@
 package com.app.ecom.integrationtest;
 
+import com.app.ecom.dto.AddressDto;
+import com.app.ecom.dto.UserDto;
+import com.app.ecom.enums.UserRole;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mysql.MySQLContainer;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,6 +27,9 @@ class UserControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Container
     static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0");
@@ -28,6 +41,28 @@ class UserControllerIT {
         dynamicPropertyRegistry.add("spring.datasource.password",mySQLContainer::getPassword);
     }
 
+    @Test
+    void testCreateUser() throws Exception{
+        AddressDto addressDto = new AddressDto(
+                "Abbey Street",
+                "Dublin",
+                "Leinster",
+                "Ireland",
+                "D15KFT7"
+        );
+        UserDto userDto = new UserDto();
+        userDto.setFirstName("John");
+        userDto.setEmail("john@gmail.com");
+        userDto.setAddress(addressDto);
+
+        mockMvc.perform(
+                post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName").value("John"))
+                .andExpect(jsonPath("$.email").value("john@gmail.com"));
+    }
 
 
 
